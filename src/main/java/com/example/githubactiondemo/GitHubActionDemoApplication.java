34380ea0f84ed1f4e7368bba94d13ab3a2348791
NikoP93 <nipe0001@stud.kea.dk>
2024-02-27 14:24:20 +0100

    Added controller stuff

--- src/main/java/com/example/githubactiondemo/GitHubActionDemoApplication.java
+++ src/main/java/com/example/githubactiondemo/GitHubActionDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitHubActionDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GitHubActionDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubActionDemoApplication.class, args);
+    }
 
 }
